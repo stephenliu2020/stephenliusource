5884bc2d9a8c928e5010e45dd85c7c5d5418a8e7
sliu <stephen.gta@gmail.com>
2020-05-03 16:46:07 -0400

    commit work flow

---
+++ WorkFlowControl/main.cpp
@@ -0,0 +1,33 @@
+#include <QQmlApplicationEngine>
+#include <QQmlComponent>
+#include <QQuickView>
+#include <QtWidgets/QApplication>
+#include "compactcontroller.h"
+QObject *object;
+int main(int argc, char *argv[])
+{
+
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+    qmlRegisterType<CompactController>("an.qml.Controls", 1, 0, "ACompactControllerItem");
+
+    QQmlApplicationEngine engine;
+
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+    if (engine.rootObjects().isEmpty())
+        return -1;
+    object=engine.rootObjects()[0];
+   // QQmlComponent component(qobject_cast<QObject*>(&engine));
+   // object = component.create();
+
+
+    return app.exec();
+}
